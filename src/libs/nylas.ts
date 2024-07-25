8cdff06da1e353d79b95ef777a7a02ad4bcc0c30
dejwid <dawid.paszko@gmail.com>
2024-07-25 20:59:16 +0200

    fixes callback url

--- src/libs/nylas.ts
+++ src/libs/nylas.ts
@@ -2,7 +2,7 @@ import Nylas from "nylas";
 
 export const nylasConfig = {
   clientId: process.env.NYLAS_CLIENT_ID,
-  callbackUri: "http://localhost:3000/api/oauth/exchange",
+  callbackUri: process.env.NEXT_PUBLIC_URL + "/api/oauth/exchange",
   apiKey: process.env.NYLAS_API_KEY,
   apiUri: process.env.NYLAS_API_URI,
 };
